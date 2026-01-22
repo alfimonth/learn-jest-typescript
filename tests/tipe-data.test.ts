1f4e003c103472b99a10817519a97e20dfc864bf
alfimonth <alvy1610@gmail.com>
2026-01-23 06:33:13 +0700

    feat(test): data type

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data type', () => {
+    it('should must declare ', () => {
+        let name: string = 'hanif daffa';
+        let balance: number = 1000;
+        let isVip: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
